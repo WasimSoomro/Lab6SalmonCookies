49485e89651503b77538b747cd2eec829149991e
WASIM SOOMRO <soomrowasimw@gmail.com>
2023-05-08 18:57:27 -0600

    Set up the structure

---
+++ app.js
@@ -0,0 +1,29 @@
+let Seattle = {
+MinCust: '23',
+MaxCust: '65',
+AvgCookie: '6.3',
+}
+
+let Tokyo = {
+  MinCust: '3',
+  MaxCust: '24',
+  AvgCookie: '1.2',
+}
+
+let Dubai = {
+  MinCust: '11',
+  MaxCust: '38',
+  AvgCookie: '3.7',
+}
+
+let Paris = {
+  MinCust: '20',
+  MaxCust: '38',
+  AvgCookie: '2.3',
+}
+
+let Lima = {
+  MinCust: '2',
+  MaxCust: '16',
+  AvgCookie: '4.6',
+}
