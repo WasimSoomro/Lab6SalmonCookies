ee2b4160f6f16540921d92e69dfd4e191279d8b8
WASIM SOOMRO <soomrowasimw@gmail.com>
2023-05-09 23:25:36 -0600

    Saved as a list for now

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-let table = document.getElementById('salesTable');
+// let table = document.getElementById('salesTable');
 
 function Store(name, minCust, maxCust, avgCookie) {
   this.name = name;
